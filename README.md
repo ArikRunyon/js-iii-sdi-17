372c75a23537dbca0766c451dfb26d035703e701
ArikRunyon <arikrunyon713@hotmail.com>
2023-05-23 13:42:46 -0400

    done round 1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # js-iii-sdi-17
+
+# This is a simple to do list webpage, with a scooby doo theme.
+# You simply type into the box and click the 'add clue' button to add a task.
+# When you complete a task, you just click the 'investigated' button to remove it.
--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 let mainButton = document.querySelector('#mainButton');
 let mainList = document.querySelector('#mainList');
+let completedList = document.querySelector('#completedList');
 let mainInput = document.querySelector('#mainInput');
-
+// localStorage.clear();
+let storageList = localStorage.getItem('list') ? JSON.parse(localStorage.getItem('list')) : [];
+let completedStorageList = localStorage.getItem('completedList') ? JSON.parse(localStorage.getItem('completedList')) : [];
 var userInput = '';
 mainInput.maxLength = 30;
 
 document.addEventListener('click', deleteLine);
 
+storageList.forEach(addActiveLine);
+completedStorageList.forEach(addCompletedLine);
+
 mainButton.addEventListener('click', (event) => {
     userInput = document.getElementById('mainInput').value;
-    let newListItem = document.createElement('li');
-    newListItem.innerHTML = `${userInput} <button class="deleteLine">Investigated!</button>`;
-    mainList.appendChild(newListItem);
+    storageList.push(`${userInput} <button class="deleteLine">Investigated!</button>`);
+    localStorage.setItem('list', JSON.stringify(storageList));
+    addActiveLine(storageList[storageList.length - 1]);
+    document.getElementById('mainInput').value = '';
 });
+
+function addActiveLine(input) {
+    let newListItem = document.createElement('li');
+    newListItem.innerHTML = input;
+    mainList.appendChild(newListItem);
+}
+
+function addCompletedLine(input) {
+    let newListItem = document.createElement('li');
+    newListItem.innerHTML = input;
+    completedList.appendChild(newListItem);
+}
 
 function deleteLine(event) {
     let element = event.target;
-    if (element.className == 'deleteLine') {
+    if (element.className == 'deleteLine' && element.parentNode.parentNode.className == 'ActiveList') {
+        completedStorageList.push(`${element.parentNode.innerHTML}`);
+        localStorage.setItem('completedList', JSON.stringify(completedStorageList));
+        addCompletedLine(completedStorageList[completedStorageList.length - 1]);
         event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+        storageList.splice(storageList.indexOf(element.parentNode.innerHTML), 1);
+        localStorage.setItem('list', JSON.stringify(storageList));
+    }
+
+    if (element.className == 'deleteLine' && element.parentNode.parentNode.className == 'CompletedList') {
+        event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+        completedStorageList.splice(completedStorageList.indexOf(element.parentNode.innerHTML), 1);
+        localStorage.setItem('completedList', JSON.stringify(completedStorageList));
     }
 }
+
